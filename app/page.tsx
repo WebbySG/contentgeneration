c3904a91021908ee6e5907fab6f88aeff1be1465
arifkoyani <arifkoyani@gmail.com>
2025-12-23 00:05:51 +0500

    setup project using Nextjs using javascript

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1 className="text-4xl font-bold">contentgeneration.webbysg.com</h1>
+      <h1 className="text-4xl font-bold">Content Generation  By Webbysg </h1>
     </div>
   );
 }
